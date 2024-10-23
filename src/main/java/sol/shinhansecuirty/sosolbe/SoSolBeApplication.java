56bf95dffb190eb143632d7cb96f466acdb5cdfc
Ho-Da-Dak <godqhr721@gmail.com>
2024-10-23 17:20:11 +0900

    init: soSOL project

---
+++ src/main/java/sol/shinhansecuirty/sosolbe/SoSolBeApplication.java
@@ -0,0 +1,13 @@
+package sol.shinhansecuirty.sosolbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoSolBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoSolBeApplication.class, args);
+    }
+
+}
