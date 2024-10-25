5b9c654ae3f5afcdafede0840160931b184d6296
LimSeHyeon <dlatpgus1205@naver.com>
2024-10-25 17:45:46 +0900

    refactor: 주석 제거

--- src/main/java/sol/shinhansecuirty/sosolbe/Service/TradeService.java
+++ src/main/java/sol/shinhansecuirty/sosolbe/Service/TradeService.java
@@ -29,33 +29,22 @@ public class TradeService {
     public BuyStockResponseDTO updateAccount(BankUpdateDTO bankUpdateDTO) {
         //계좌 정보 조회
         Account myBank = accountRepository.findByAccountNumAndType(bankUpdateDTO.getAccountNum(), "은행");
-        System.out.println(bankUpdateDTO.getAccountNum());
-        System.out.println("차감 전 : " + myBank.getBalance());
         //은행계좌에서 차감
         myBank.setBalance(myBank.getBalance() - bankUpdateDTO.getTradePrice());
-        System.out.println("차감 후 : " + myBank.getBalance());
         //잔액에서 잔돈 추출, 은행계좌 잔돈 차감
         int divisor = myBank.getBalanceSize();
         int small = myBank.getBalance() % divisor;
         myBank.setBalance((myBank.getBalance()/divisor)*1000);
-        System.out.println("잔돈 : " + small);
-        System.out.println("잔돈 추출 후 은행 : " + myBank.getBalance());
         accountRepository.save(myBank);
 
         //증권계좌 가져와서 잔액 업데이트
         Account mySecurity = accountRepository.findByUserAndType(myBank.getUser(), "증권");
-        System.out.println("증권 잔액 변동 전 : " + mySecurity.getBalance());
         mySecurity.setBalance(mySecurity.getBalance() + small);
-        System.out.println("증권 잔액 변동 후 : " + mySecurity.getBalance());
 
         //잔돈 불러와서 업데이트
         SmallChange smallChange = smallChangeRepository.findByAccount(mySecurity);
-        System.out.println("잔돈 변동 전 : " + smallChange.getCurrentBalance());
         smallChange.setCurrentBalance(smallChange.getCurrentBalance() + small);
-        System.out.println("변동 후 : " + smallChange.getCurrentBalance());
-        System.out.println("토탈 잔돈 변동 전 : " + smallChange.getTotal());
         smallChange.setTotal(smallChange.getTotal() + small);
-        System.out.println("토탈 잔돈 변동 후 : " + smallChange.getTotal());
 
         //목표 주식 정보 조회
         Target target = targetRepository.findByUser(mySecurity.getUser());
